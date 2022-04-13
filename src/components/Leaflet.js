eb587208e65411ac4b8bd14502f2a4b66253eef6
callum09 <hello@callumoberholzer.co.za>
2022-04-13 13:40:19 +0200

    Adjusted sizing of buttons and cookie policy

--- src/components/Leaflet.js
+++ src/components/Leaflet.js
@@ -9,10 +9,7 @@ const Map = () => {
         <div className="leaflet-container">
             <Sidebar_left />
         <MapContainer center={[51.505, -0.09]} zoom={13}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             <Marker position={[51.505, -0.09]}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
