2f40dfd4ad72d3478c16b4b18d0189a922ffacb2
callum09 <hello@callumoberholzer.co.za>
2022-04-15 07:08:35 +0200

    Spelling change

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -300,7 +300,7 @@ const Sidebar = (props) => {
               <option value="Water">Water</option>
               <option value="Communications">Communications (Airtime / data)</option>
               <option value="Electricity">Electricity (charging)</option>
-              <option value="Missing Person/'s">Missing person/'s</option>
+              <option value="Missing Person's">Missing person's</option>
               <option value="Other">Other</option>
             </select><br />
             <label>Need description</label>
@@ -336,7 +336,7 @@ const Sidebar = (props) => {
               <option value="Water">Water</option>
               <option value="Communications">Communications (Airtime / data)</option>
               <option value="Electricity">Electricity (charging)</option>
-              <option value="Missing Person/'s">Missing person/'s</option>
+              <option value="Missing Person/s">Missing person/s</option>
               <option value="Other">Other</option>
             </select><br />
             <label>Resource description</label>
