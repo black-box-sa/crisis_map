489e2d5a24d2353030f76aa548b06cb9a47f893a
PhakamaniXulu <66253043+PhakamaniXulu@users.noreply.github.com>
2022-04-21 16:12:23 +0200

    filter bar import

--- src/components/Leaflet.js
+++ src/components/Leaflet.js
@@ -8,6 +8,7 @@ import Sidebar_left from "./sidebar";
 import Footer_thin from "./Footer_thin";
 import needMarker from'./map-marker-alt-red.svg';
 import resourceMarker from'./map-marker-alt-green.svg';
+import FilterBar from './filterbar'
 import 'leaflet/dist/leaflet.css'
 
 Geocode.setApiKey(process.env.REACT_APP_GOOGLE_APIKEY);
@@ -154,6 +155,7 @@ const Map = () => {
     return (
         <div className="leaflet-container">
             <Sidebar_left setCenter={setCenter} needInfo={needInfo} sidebar={sidebar} assist_address={address} sidebarTrigger={sidebarTrigger} lognewTrigger={lognewTrigger} getResources={getResources} openLastNeeds={openLastNeeds} getNeeds={getNeeds} need={need} type={type} user_type={user_type} setUserType={setUserType} assists={assists} getAssist={getAssist} />
+            <FilterBar/>
         <MapContainer center={center} zoom={12}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
             {
