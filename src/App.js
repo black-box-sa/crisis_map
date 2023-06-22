5381b5cf34cc0215ac37b16ea6ba5793b74a0c63
Nzuzo_Malinga <nzuzomal@gmail.com>
2023-06-22 09:21:15 +0200

    creating router for cape town page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Map from './components/Leaflet';
+import CapeTownMap from './components/CapeTownMap';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Map />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Map/>}/>
+        <Route path="/capetown" element={<CapeTownMap/>}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
